e18d6a600b8ae4a2944bf0904f9172788b2287b2
sharathnair9999 <61535475+sharathnair9999@users.noreply.github.com>
2021-08-22 19:16:09 +0530

    initial commit

---
+++ firebase.js
@@ -0,0 +1,14 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyAMb0AiF2VzMS7PmAuJkVIeLV17AK7SQe0",
+    authDomain: "clone-b9d36.firebaseapp.com",
+    projectId: "clone-b9d36",
+    storageBucket: "clone-b9d36.appspot.com",
+    messagingSenderId: "744059082930",
+    appId: "1:744059082930:web:39d3167ec4141a956791ab",
+    measurementId: "G-DBKEWEVB1N"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var db = firebase.firestore();
---
+++ script.js
@@ -0,0 +1,78 @@
+function getItems() {
+  db.collection("items")
+    .get()
+    .then((querySnapshot) => {
+      let items = [];
+      querySnapshot.forEach((doc) => {
+        items.push({
+          id: doc.id,
+          image: doc.data().image,
+          name: doc.data().name,
+          make: doc.data().make,
+          rating: doc.data().rating,
+          price: doc.data().price,
+        });
+      });
+      generateItems(items);
+    });
+}
+
+function addToCart(item){
+ let cartItem =  db.collection('cart-items').doc(item.id);
+
+ cartItem.get()
+ .then(function(doc){
+   if(doc.exists){
+     cartItem.update({
+       quantity: doc.data().quantity+1
+     })
+   }
+   else{
+     cartItem.set({
+      image : item.image,
+      name : item.name,
+      make : item.make,
+      price : item.price,
+      rating : item.rating,
+      quantity: 1
+    })
+   }
+ })
+ 
+}
+
+function generateItems(items) {
+  let itemsHTML = "";
+  items.forEach((item) => {
+    let doc = document.createElement("div");
+    doc.classList.add("main-product", "mr-5");
+    doc.innerHTML = `
+    <div class  ='product-image w-48 h-52 bg-white rounded-lg p-4'>
+      <img class = 'w-full h-full object-contain' src="${item.image}" alt="">
+    </div>
+    <div class = 'product-name text-gray-700 font-bold mt-2 text-sm'>
+      ${item.name}
+    </div>
+    <div class = 'product-make text-gray-700 font-bold text-green-700'>
+      ${item.make}
+    </div>
+    <div class = 'product-rating text-yellow-300 font-bold my-1'>
+      ⭐⭐⭐⭐⭐ ${item.rating}
+    </div>
+    <div class = 'product-price font-bold text-gray-700 text-lg'>
+      ${numeral(item.price).format('$0,0.00')}
+    </div>
+    
+    `
+
+    let addToCartEl = document.createElement('div')
+    addToCartEl.classList.add('add-to-cart', 'w-28', 'h-8', 'text-md', 'items-center', 'flex', 'justify-center', 'bg-yellow-500', 'rounded', 'text-white' ,'cursor-pointer', 'hover:bg-yellow-600')
+    addToCartEl.innerText = 'Add to Cart';
+    addToCartEl.addEventListener('click', function(){
+      addToCart(item)
+    })
+    doc.appendChild(addToCartEl);
+    document.querySelector(".main-section-products").appendChild(doc)
+})
+}
+getItems();
